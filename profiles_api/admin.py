fc39b3801e027944b0e29fc999b814c61d509a93
shubhankarsaxena1997 <shubhankarsaxena10@gmail.com>
2025-01-31 11:51:20 +0530

    added few fixes

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,5 +1,11 @@
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.admin import TokenAdmin
 from django.contrib import admin
 from .models import *
 
+class CustomTokenAdmin(TokenAdmin):
+    search_fields = ['name']  # Assuming you want to search by name
+
+admin.site.register(Token, CustomTokenAdmin)
 
 admin.site.register(UserProfile)
